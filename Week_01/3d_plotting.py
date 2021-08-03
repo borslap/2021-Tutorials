831a74853e872f2fa66676c33c734ea3fe1284d0
Alex-Muirhead <alexandermuirhead@hotmail.com>
2021-08-03 11:41:28 +1000

    Add ICT session work

---
+++ Week_01/3d_plotting.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: alexa
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-1, +1, 9)
+y = np.linspace(-1, +1, 9)
+
+X, Y = np.meshgrid(x, y)
+Z = X**2 + Y**2
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+ax.plot_surface(X, Y, Z)
+plt.show()
---
+++ Week_01/efficient_calculations.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+@author: alexa
+"""
+
+import numpy as np
+
+
+def fact(n):
+    """Calculate the value of n factorial.""" 
+    # 4! = 4*3*2*1
+    product = 1
+    for i in range(1, n+1):
+        product *= i
+    return product
+
+
+def exptaylor(n, x):
+    """Taylor series expansion of e^x """
+    output = np.zeros_like(x)
+    for i in range(n):
+        output += x**i / fact(i)
+    return output
+
+    # output = 
+    #   x / 1
+    # + (x)*x / (1*2)
+    # + (x*x)*x / (1*2*3)
+    
+def better_exptaylor(n, x):
+    """Better Taylor series expansion of e^x """
+    term = 1
+    output = term
+    for i in range(1, n):
+        term   *= x / i
+        output += term
+    return output
