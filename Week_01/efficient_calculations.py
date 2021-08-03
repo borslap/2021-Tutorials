1da40c8d3d301cc3ea4043a9a7b3a6c6d58f6244
Alex-Muirhead <alexandermuirhead@hotmail.com>
2021-08-03 12:43:39 +1000

    Add descriptive comments

--- Week_01/efficient_calculations.py
+++ Week_01/efficient_calculations.py
@@ -7,8 +7,8 @@ import numpy as np
 
 
 def fact(n):
-    """Calculate the value of n factorial.""" 
-    # 4! = 4*3*2*1
+    """Calculate the value of n factorial."""
+    # n! = n*(n-1)*...*2*1
     product = 1
     for i in range(1, n+1):
         product *= i
@@ -16,22 +16,50 @@ def fact(n):
 
 
 def exptaylor(n, x):
-    """Taylor series expansion of e^x """
-    output = np.zeros_like(x)
+    """Taylor series expansion of e^x
+
+    NOTE:
+        This will fail with x as a np.ndarray when
+        n > 21, due to a quirk with division by
+        large numbers (21! = 51090942171709440000).
+        Just another reason to avoid this implemenation!
+    """
+    output = 0
     for i in range(n):
         output += x**i / fact(i)
     return output
 
-    # output = 
-    #   x / 1
-    # + (x)*x / (1*2)
-    # + (x*x)*x / (1*2*3)
-    
+
 def better_exptaylor(n, x):
-    """Better Taylor series expansion of e^x """
+    """
+    Better Taylor series expansion of e^x
+
+    term 0: 1
+    term 1: x**1 / 1! == (term 0) * x
+    term 2: x**2 / 2! == (term 1) * x / 2
+    term 3: x**3 / 3! == (term 2) * x / 3
+    """
     term = 1
     output = term
     for i in range(1, n):
         term   *= x / i
         output += term
+    return output
+
+
+def lntaylor(n, x):
+    """
+    Efficient Taylor series expansion of ln(x+1) at x=1
+
+    term 0: -1 {not included}
+    term 1:   (x-1)**1 / (2**1 * 1)
+    term 2: - (x-1)**2 / (2**2 * 2)
+    term 3:   (x-1)**3 / (2**3 * 3)
+    term 4: - (x-1)**4 / (2**4 * 4)
+    """
+    term = -1
+    output = np.log(2.)  # <- 0th term
+    for i in range(1, n):
+        term *= (1-x) / 2
+        output += term / i
     return output
